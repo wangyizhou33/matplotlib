d28d35c17edd8df28706116837fc71863c209957
Yizhou Wang <yizhouw@nvidia.com>
2019-11-22 16:56:34 -0800

    init commit

---
+++ earthquake.py
@@ -0,0 +1,76 @@
+import urllib
+import numpy as np
+import matplotlib
+matplotlib.rcParams['toolbar'] = 'None'
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from  matplotlib.animation import FuncAnimation
+
+# -> http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+feed = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"
+
+# Significant earthquakes in the last 30 days
+# url = urllib.request.urlopen(feed + "significant_month.csv")
+
+# Magnitude > 4.5
+# url = urllib.request.urlopen(feed + "4.5_month.csv")
+
+# Magnitude > 2.5
+# url = urllib.request.urlopen(feed + "2.5_month.csv")
+
+# Magnitude > 1.0
+url = urllib.request.urlopen(feed + "1.0_month.csv")
+
+# Reading and storage of data
+data = url.read().decode('UTF-8', 'ignore')
+data = data.split('\n')[+1:-1]
+
+# data = data.split(b'\n')[+1:-1]
+E = np.zeros(len(data), dtype=[('position',  float, 2),
+                               ('magnitude', float, 1)])
+
+for i in range(len(data)):
+    row = data[i].split(',')
+    E['position'][i] = float(row[2]),float(row[1])
+    E['magnitude'][i] = float(row[4])
+
+fig = plt.figure(figsize=(14,10))
+ax = plt.subplot(1,1,1)
+
+earth = Basemap(projection='mill')
+
+earth.drawcoastlines(color='0.50', linewidth=0.25)
+earth.fillcontinents(color='0.95')
+
+P = np.zeros(50, dtype=[('position', float, 2),
+                         ('size',     float, 1),
+                         ('growth',   float, 1),
+                         ('color',    float, 4)])
+scat = ax.scatter(P['position'][:,0], P['position'][:,1], P['size'], lw=0.5,
+                  edgecolors = P['color'], facecolors='None', zorder=10)
+
+def update(frame):
+    current = frame % len(E)
+    i = frame % len(P)
+
+    P['color'][:,3] = np.maximum(0, P['color'][:,3] - 1.0/len(P))
+    P['size'] += P['growth']
+
+    magnitude = E['magnitude'][current]
+    P['position'][i] = earth(*E['position'][current])
+    P['size'][i] = 5
+    P['growth'][i]= np.exp(magnitude) * 0.1
+
+    if magnitude < 6:
+        P['color'][i]    = 0,0,1,1
+    else:
+        P['color'][i]    = 1,0,0,1
+    scat.set_edgecolors(P['color'])
+    scat.set_facecolors(P['color']*(1,1,1,0.25))
+    scat.set_sizes(P['size'])
+    scat.set_offsets(P['position'])
+    return scat,
+
+
+animation = FuncAnimation(fig, update, interval=10)
+plt.show()
